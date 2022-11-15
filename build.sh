d79bb72cecd0a297ee1acf8ddaafa6fdac9872da
Liu Xiaoyi <circuitcoder0@gmail.com>
2022-11-15 21:19:23 +0800

    Bump version, add signature

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@ TOPS="circt"
 
 build() {
   echo "Building $1..."
-  nix-build $1.nix -o $1.out
-  REALPATH=$(readlink -f $1.out)
-  echo "Copying to cache: $REALPATH"
-  nix copy --to "s3://nix?profile=nix-upload&scheme=https&endpoint=minio.inner.fi.c-3.moe" $REALPATH
+  nix-build $1.nix --no-out-link
+  STOREPATH=$(nix-build ./$1.nix)
+  DRVPATH=$(nix-instantiate ./$1.nix)
+  echo "Copying to cache: $DRVPATH"
+  nix store sign --recursive -k ../cache-priv-key.pem $DRVPATH
+  nix copy --derivation --to "s3://nix?profile=nix-upload&scheme=https&endpoint=minio.inner.fi.c-3.moe" $DRVPATH
 }
 
 for TOP in $TOPS; do
