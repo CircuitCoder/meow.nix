8e11976a9c4991ea727f02c4bc167a5fa325949e
Liu Xiaoyi <circuitcoder0@gmail.com>
2022-11-15 22:06:37 +0800

    Only upload store

--- build.sh
+++ build.sh
@@ -7,10 +7,7 @@ build() {
   nix-build $1.nix --no-out-link
   STOREPATH=$(nix-build ./$1.nix)
   DRVPATH=$(nix-instantiate ./$1.nix)
-  echo "Copying to cache: $DRVPATH, $STOREPATH"
-  # nix store sign --recursive -k ../cache-priv-key.pem $STOREPATH
-  # nix store sign --recursive -k ../cache-priv-key.pem $DRVPATH
-  nix copy --derivation --to "s3://nix?profile=nix-upload&scheme=https&endpoint=minio.inner.fi.c-3.moe&secret-key=../cache-priv-key.pem" $DRVPATH
+  echo "Copying to cache: $STOREPATH"
   nix copy --to "s3://nix?profile=nix-upload&scheme=https&endpoint=minio.inner.fi.c-3.moe&secret-key=../cache-priv-key.pem" $STOREPATH
 }
 
